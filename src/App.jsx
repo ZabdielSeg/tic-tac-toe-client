cc5297bb7852f8646e85bfed25f61fd22d2ca864
Zabdiel Segura <zabdiel-segura@hotmail.com>
2023-05-04 09:01:24 -0600

    Config react router dom

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react'
+import Login from './pages/Login';
+import Lobby from './pages/Lobby';
+import NavBar from './components/NavBar/NavBar';
+import GameRoom from './pages/GameRoom';
+import { Route, Routes } from 'react-router-dom';
+const socket = io('http://localhost:4000')
+
+function App() {
+  const [user, setUser] = useState({});
+  const [allConnectedUsers, setAllConnectedUsers] = useState([]);
+
+  const getTheUser = (user) => {
+    setUser(user);
+  }
+
+  const addUser = (newUser) => {
+    setAllConnectedUsers([...allConnectedUsers, newUser])
+  }
+
+  useEffect(() => {
+    socket.on('userInfo', data => {
+      setAllConnectedUsers([...allConnectedUsers, data.userData])
+    });
+  }, [user, socket])
+
+  return (
+    <>
+      <NavBar theUser={user} />
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Login getTheUser={getTheUser} addUser={addUser} socket={socket} />} />
+          <Route path='/lobby' element={<Lobby socket={socket} theUser={user} />} />
+          <Route path='/game-room' element={<GameRoom theUser={user} socket={socket} getTheUser={getTheUser} />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
